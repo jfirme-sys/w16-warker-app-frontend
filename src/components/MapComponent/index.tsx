ab03c8c0fcafb6d328d6e63b96de47bfe7767017
Juliano Firme <julianofirme23@gmail.com>
2022-01-23 17:44:02 -0300

    Fix map component icons

--- src/components/MapComponent/index.tsx
+++ src/components/MapComponent/index.tsx
@@ -1,10 +1,9 @@
 import React, { useContext, useState } from 'react';
-import { ipDataContext } from 'providers/ipDataProvider';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+// import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import L, { LatLngExpression, Map } from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 import './map.css'
-const icon = require( 'assets/marker-icon.png');
+const icon = require('assets/marker-icon.png');
 const iconShadow = require('assets/marker-shadow.png');
 
 let DefaultIcon = L.icon({
@@ -15,8 +14,7 @@ let DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 function MapComponent() {
-  const { ipData } = useContext(ipDataContext);
-  const position = [ipData.location.lat, ipData.location.lng] as LatLngExpression
+  const position = [0, 0] as LatLngExpression
   const [map, setmap] = useState<Map>();
   if (map) {
     map.setView(position);
@@ -24,15 +22,15 @@ function MapComponent() {
 
   return (
     <>
-      <MapContainer center={position} zoom={15} scrollWheelZoom={true} whenCreated={setmap}>
+      {/* <MapContainer center={position} zoom={15} scrollWheelZoom={true} whenCreated={setmap}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
-          
+
         </Marker>
-      </MapContainer>
+      </MapContainer> */}
     </>
   );
 }
