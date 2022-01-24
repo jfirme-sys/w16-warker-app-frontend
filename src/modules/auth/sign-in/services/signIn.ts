4ddbe5bc61e5c21a329612b08a98ad453e04585d
Juliano Firme <julianofirme23@gmail.com>
2022-01-24 20:40:29 -0300

    Login request headers

--- src/modules/auth/sign-in/services/signIn.ts
+++ src/modules/auth/sign-in/services/signIn.ts
@@ -1,8 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
 import { UserCredentials } from '../models';
 
+const token = process.env.REACT_APP_AUTH_TOKEN
+
+axios.defaults.withCredentials = true
+
 const login = async (credentials: UserCredentials) => {
-  const response = await axios.post("https://warker-api.herokuapp.com/api/login", credentials)
+  const response = await axios.post("https://warker-api.herokuapp.com/api/login", 
+    credentials, {
+    headers: { 
+      'Authorization': `Bearer ${token}`, 
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
   console.log(response);
 }
 
