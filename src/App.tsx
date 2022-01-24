81f6429d873ac6b16942cc3811c9276b37f45dd9
Juliano Firme <julianofirme23@gmail.com>
2022-01-24 16:49:37 -0300

    Create new login page using Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "pages/Home";
-import Login from "pages/Login";
+import SignIn from "pages/sign-in";
 import 'style/styles.css'
+import SignUp from 'pages/sign-up';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
