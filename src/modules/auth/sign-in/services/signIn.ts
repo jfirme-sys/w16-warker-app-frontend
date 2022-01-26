d5cf542db8eab4adc1062ca5117115daecd30b9c
Juliano Firme <julianofirme23@gmail.com>
2022-01-26 15:18:12 -0300

    Fix: remove headers

--- src/modules/auth/sign-in/services/signIn.ts
+++ src/modules/auth/sign-in/services/signIn.ts
@@ -1,18 +1,11 @@
 import axios from 'axios';
 import { UserCredentials } from '../models';
 
-const token = process.env.REACT_APP_AUTH_TOKEN
-
-axios.defaults.withCredentials = true
 
 const login = async (credentials: UserCredentials) => {
   const response = await axios.post("https://warker-api.herokuapp.com/api/login", 
-    credentials, {
-    headers: { 
-      'Authorization': `Bearer ${token}`, 
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
+    credentials
+  )
   console.log(response);
 }
 
