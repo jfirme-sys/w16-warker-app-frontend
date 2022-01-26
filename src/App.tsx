a222ba377ddca4ec98168fd2e23e17015ffed605
Juliano Firme <julianofirme23@gmail.com>
2022-01-26 15:55:19 -0300

    Auth context

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import Home from "pages/Home";
 import SignIn from "pages/sign-in";
 import 'style/styles.css'
 import SignUp from 'pages/sign-up';
+import AuthProvider from 'providers/authProvider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/modules/auth/sign-in/services/signIn.ts
+++ src/modules/auth/sign-in/services/signIn.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
-import { UserCredentials } from '../models';
+import { UserCredentials, UserLoginResponse } from '../models';
 
 
-const login = async (credentials: UserCredentials) => {
+const login = async (credentials: UserCredentials): Promise<UserLoginResponse> => {
   const response = await axios.post("https://warker-api.herokuapp.com/api/login", 
     credentials
   )
-  console.log(response);
+  console.log(response)
+  return {
+    ...response.data,
+    status: response.status
+  }
 }
 
 export { login }
